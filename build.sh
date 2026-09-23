c95edc596c74749f4a02b67016e9788ced532421
Rommmmaha <Rommmmaha@gmail.com>
2026-09-23 11:36:11 +0300

    feat(build): add --install flag to install vsix via code

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 set -e
 cd "$(dirname "$0")"
 
+INSTALL=0
+for arg in "$@"; do
+  case "$arg" in
+    --install) INSTALL=1 ;;
+    *) echo "Unknown option: $arg" >&2; exit 1 ;;
+  esac
+done
+
 bunx tsc
 
 rm -rf staging
@@ -15,3 +23,7 @@ cd staging && zip -r ../r-qol.vsix . && cd ..
 rm -rf staging
 
 echo "Built r-qol.vsix ($(du -h r-qol.vsix | cut -f1))"
+
+if [ "$INSTALL" -eq 1 ]; then
+  code --install-extension r-qol.vsix
+fi
